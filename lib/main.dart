d123c0422c33962076c76ad010e17c6b517e884c
fwdAndroid <fwdkaleem@gmail.com>
2023-01-10 19:50:58 +0500

    Animated Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:order_management/mains_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +27,18 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: AnimatedSplashScreen(
+        nextScreen: MainScreen(),
+        splash: Image.asset(
+          'assets/splash.png',
+          fit: BoxFit.cover,
+        ),
+        duration: 3000,
+        splashIconSize: 400,
+        splashTransition: SplashTransition.slideTransition,
+        pageTransitionType: PageTransitionType.fade,
+        backgroundColor: Colors.white,
+      ),
     );
   }
 }
