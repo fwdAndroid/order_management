d59acd9716ac1b3ab08fcddde356012f9f00192c
fwdAndroid <fwdkaleem@gmail.com>
2023-01-17 21:15:15 +0500

    Change

--- lib/mains_screen.dart
+++ lib/mains_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +12,72 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Image.asset(
+            'assets/splash.png',
+            height: 250,
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.business),
+              title: Text("Bussiness Manager "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.recycling_outlined),
+              title: Text("Regional Sales Manager "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.exposure_zero_outlined),
+              title: Text("Zonal Sales Manager "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.area_chart),
+              title: Text("Area Sales Manager "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.terminal),
+              title: Text("Territory Sales Manager "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.local_post_office),
+              title: Text("Sales Officer "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.discount),
+              title: Text("Distributer Officer "),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.shop),
+              title: Text("Retailer App"),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
