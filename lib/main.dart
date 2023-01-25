e41af6e424516f103ecbc89dc5eb39a425482cb6
fwdAndroid <fwdkaleem@gmail.com>
2023-01-26 01:30:48 +0500

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:order_management/mains_screen.dart';
 import 'package:page_transition/page_transition.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
