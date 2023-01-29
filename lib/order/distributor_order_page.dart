cc29fc0ac641050dd31b4171fcee9411c9da2a7b
fwdAndroid <fwdkaleem@gmail.com>
2023-01-30 02:39:14 +0500

    Distributor Main UI REady

---
+++ lib/order/distributor_order_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DistributorOrderPage extends StatefulWidget {
+  const DistributorOrderPage({super.key});
+
+  @override
+  State<DistributorOrderPage> createState() => _DistributorOrderPageState();
+}
+
+class _DistributorOrderPageState extends State<DistributorOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/main_distributor_page.dart
+++ lib/pages/main_distributor_page.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:order_management/mains_screen.dart';
+import 'package:order_management/order/distributor_order_page.dart';
 
 class MainDistributorPage extends StatefulWidget {
   const MainDistributorPage({super.key});
@@ -11,6 +16,132 @@ class MainDistributorPage extends StatefulWidget {
 class _MainDistributorPageState extends State<MainDistributorPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            TextButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut().then((value) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (builder) => MainScreen()));
+                  });
+                },
+                child: Text(
+                  "LogOut",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ))
+          ],
+          title: Text("Distributor"),
+          backgroundColor: Colors.purple,
+        ),
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("distrubutor")
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              var ds = snapshot.data;
+
+              if (!snapshot.hasData) {
+                Center(
+                  child: Text("No Product Listed"),
+                );
+              }
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      child: Card(
+                        elevation: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Center(
+                              child: Text(
+                                "Distributor Profile Info:",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                            ),
+                            Divider(),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Distributor Name:",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 17),
+                            ),
+                            Text("Name:" + ds['name']),
+                            Divider(),
+                            Text(
+                              "Distributor Email:",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 17),
+                            ),
+                            Text("email" + ds['email']),
+                            Divider(),
+                            Text(
+                              "Distributor Area:",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 17),
+                            ),
+                            Text(ds['area']),
+                            Divider(),
+                            Text(
+                              "Distributor Password:",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 17),
+                            ),
+                            Text(ds['password']),
+                            // trailing: TextButton(
+                            //     onPressed: () {
+                            //       Navigator.push(
+                            //           context,
+                            //           MaterialPageRoute(
+                            //               builder: (builder) => ProductDetail(
+                            //                     area: widget.area,
+                            //                     name: widget.name,
+                            //                     uuid: snap['uuid'],
+                            //                     rate: snap['rate'],
+                            //                     dimension: snap['dimensions'],
+                            //                     pcs: snap['pcs'],
+                            //                     productname: snap['productname'],
+                            //                   )));
+                            //     },
+                            //     child: Text("View")),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print(ds['email']);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (builder) => DistributorOrderPage()));
+                    },
+                    child: Text("View Orders"),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        fixedSize: Size(200, 50),
+                        shape: StadiumBorder()),
+                  )
+                ],
+              );
+            }));
   }
 }
